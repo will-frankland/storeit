fc6a9a7da92de46659e15462b2aa2fa0c6d3bdc1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-11-18 17:46:58 -0500

    setup shadcn form boilerplate

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -38,8 +38,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             className="h-auto w-[200px] lg:w-[250px]"
           />
         </div>
+        {children}
       </section>
-      {children}
     </div>
   );
 };
