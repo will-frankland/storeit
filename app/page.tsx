f3dfd1675497b0abef42e96e285878c29d60a7de
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-11-18 17:13:07 -0500

    setup tailwind and global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">StoreIt - the only storage solution you need</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIt - the only storage solution you need</h1>
     </div>
   );
 }
