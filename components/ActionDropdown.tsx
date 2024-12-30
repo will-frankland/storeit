5f6f9441c8ad8a40424ebdaa0b22e085522540e0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-12-29 19:59:14 -0500

    add details functionality for details modal

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -26,6 +26,7 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 import { renameFile } from "@/lib/actions/file.actions";
+import { FileDetails } from "./ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -83,6 +84,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+          {value === "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
---
+++ components/ActionsModalContent.tsx
@@ -0,0 +1,35 @@
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
+import { convertFileSize, formatDateTime } from "@/lib/utils";
+
+const ImageThumbnail = ({ file }: { file: Models.Document }) => (
+  <div className="file-details-thumbnail">
+    <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+    <div className="flex-col flex">
+      <p className="subtitle-2 mb-1">{file.name}</p>
+      <FormattedDateTime date={file.$createdAt} className="caption" />
+    </div>
+  </div>
+);
+
+const DetailRow = ({ label, value }: { label: string; value: string }) => (
+  <div className="flex ">
+    <p className="file-details-label text-left">{label}</p>
+    <p className="file-details-value text-left">{value}</p>
+  </div>
+);
+
+export const FileDetails = ({ file }: { file: Models.Document }) => {
+  return (
+    <>
+      <ImageThumbnail file={file} />
+      <div className="space-y-4 px-2 pt-2">
+        <DetailRow label="Format:" value={file.extension} />
+        <DetailRow label="Size:" value={convertFileSize(file.size)} />
+        <DetailRow label="Owner:" value={file.owner.fullName} />
+        <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
+    </>
+  );
+};
